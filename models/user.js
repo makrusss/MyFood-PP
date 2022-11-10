1f05b3960ab7ed5314013941292af472cf9827a5
Markus <markus.sarsanto@gmail.com>
2022-11-10 18:21:05 +0700

    Home

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Item)
+      User.hasMany(models.Item)
       User.hasOne(models.Profile)
     }
   }
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,6 +2,16 @@ const express = require('express')
 const Controller = require('../controllers/home')
 const router = express.Router()
 
-router.use('/', Controller.renderHome)
+router.get('/', Controller.renderHome) //ada params disini
+
+router.get('/keranjang', Controller.keranjang)
+router.get('/logout')
+
+router.get('/profile/:UserId', Controller.renderProfile)
+router.get('/profile/:UserId/edit', Controller.renderEdit)
+router.post('/profile/:UserId/edit/', Controller.edit)
+
+router.get('/buy/:UserId/:ItemId',Controller.buy)
+router.get('/undo/:UserId/:ItemId',Controller.undo)
 
 module.exports = router  
